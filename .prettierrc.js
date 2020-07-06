9417c1bd4f0b4d317c96b6f1086feba101a03212
solidw <solidw@naver.com>
2020-07-07 02:22:27 +0900

    chore: No singlequote

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   printWidth: 100,
   parser: "typescript",
-  singleQuote: true,
+  singleQuote: false,
   useTabs: false, // Indent lines with tabs instead of spaces.
   printWidth: 80, // Specify the length of line that the printer will wrap on.
   tabWidth: 2, // Specify the number of spaces per indentation-level.
