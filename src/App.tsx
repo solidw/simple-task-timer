bcee71815f3233af9e1ed06036efb335ef91bf6a
solidw <solidw@naver.com>
2020-07-09 18:50:25 +0900

    style: Test localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import Timer from './components/Timer';
-import './App.css';
+import React, { useEffect } from "react";
+import Timer from "./components/Timer";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    return () =>
+      window.localStorage.setItem(
+        "asf",
+        JSON.stringify(`{something:"something"}`)
+      );
+  }, []);
   return (
     <div className="app-wrapper">
       <Timer />
