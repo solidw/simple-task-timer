181746dfb4257234612b093fa72cb4d4b4dc0646
solidw <solidw@naver.com>
2021-01-08 21:32:42 +0900

    style: Apply eslint at electron.js

--- public/electron.js
+++ public/electron.js
@@ -1,46 +1,52 @@
-const electron = require("electron");
+const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const path = require("path");
-const isDev = require("electron-is-dev");
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ 
-    width: 900, 
-    height: 680,
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
       devTools: isDev,
     },
+    show: false,
+    titleBarStyle: 'hiddenInset',
   });
-  
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
   mainWindow.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
   );
-  
+
   if (isDev) {
     mainWindow.webContents.openDevTools({ mode: 'detach' });
   }
-  
+
   mainWindow.setResizable(true);
   mainWindow.on('closed', () => (mainWindow = null));
   mainWindow.focus();
 }
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
